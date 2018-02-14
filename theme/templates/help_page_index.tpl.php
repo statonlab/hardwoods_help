8653370a3988ce56b51ffdee4823e6677bf16618
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-02-14 09:46:25 -0500

    Completed with styling

--- theme/templates/help_page_index.tpl.php
+++ theme/templates/help_page_index.tpl.php
@@ -1,18 +1,22 @@
-<?php foreach ($items as $item): ?>
-    <a href="<?php print "/node/{$item['node']->id}"; ?>">
-        <h4>
-          <?php print $item['node']->title ?>
-        </h4>
-    </a>
-
-  <?php foreach ($item['topics'] as $topic): ?>
-        <ul>
-            <li>
-                <a href="<?php print "help-tab__tab_{$topic->id}" ?>">
-                    <?php print $topic->title ?>
-                </a>
-            </li>
-        </ul>
+<div class="row">
+  <?php foreach ($items as $item): ?>
+      <div class="col-md-6">
+          <a href="<?php print "/node/{$item['node']->nid}"; ?>"
+             class="link-darker">
+              <h4>
+                <?php print $item['node']->title ?>
+              </h4>
+          </a>
+          <ul class="nav flex-column">
+            <?php foreach ($item['topics'] as $topic): ?>
+                <li class="nav-item">
+                    <a href="<?php print "help-tab__tab_{$topic->id}" ?>"
+                       class="nav-link">
+                      <?php print $topic->title ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+          </ul>
+      </div>
   <?php endforeach; ?>
-
-<?php endforeach; ?>
+</div>
